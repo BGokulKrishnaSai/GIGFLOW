2e61a3523bf07adf171defdf267350d9149671ce
Balaji Gokul Krishna Sai <149820969+BGokulKrishnaSai@users.noreply.github.com>
2026-01-12 20:53:44 +0530

    Fix all API endpoints - replace /gigs with /api/gigs in all methods

--- client/src/services/gigService.js
+++ client/src/services/gigService.js
@@ -2,33 +2,33 @@ import api from './api';
 
 const gigService = {
   getAllGigs: async (search = '') => {
-    const url = search ? `//api/gigs?search=${search}` : '/gigs';
+    const url = search ? `/api/gigs?search=${search}` : '/api/gigs';
     const response = await api.get(url);
     return response.data.data;
   },
 
   getGigById: async (gigId) => {
-    const response = await api.get/api/gigsgigs/${gigId}`);
+    const response = await api.get(`/api/gigs/${gigId}`);
     return response.data.data;
   },
 
   createGig: async (gigData) => {
-    const response = await api.post('//api/gigs', gigData);
+    const response = await api.post('/api/gigs', gigData);
     return response.data.data;
   },
 
   updateGig: async (gigId, gigData) => {
-    const response = await api.patch(`//api/gigs/${gigId}`, gigData);
+    const response = await api.patch(`/api/gigs/${gigId}`, gigData);
     return response.data.data;
   },
 
   deleteGig: async (gigId) => {
-    await api.delete(`//api/gigs/${gigId}`);
+    await api.delete(`/api/gigs/${gigId}`);
     return gigId;
   },
 
   getMyGigs: async () => {
-    const response = await api.get('//api/gigs/my/my');
+    const response = await api.get('/api/gigs/my');
     return response.data.data;
   },
 };
