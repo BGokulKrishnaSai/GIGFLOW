d8b7b673e7e5450503ece047e753e1f14bd0f269
Balaji Gokul Krishna Sai <149820969+BGokulKrishnaSai@users.noreply.github.com>
2026-01-13 14:42:11 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://gigflow-api-b177.onrender.com",
         changeOrigin: true,
       },
     },
