581d391e47a3866dab0243e1b2c826e11fa7cb14
Balaji Gokul Krishna Sai <149820969+BGokulKrishnaSai@users.noreply.github.com>
2026-01-14 09:54:15 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'          // ← App already has Router
+import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
@@ -8,7 +8,7 @@ import { store } from './redux/store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />                          // ← No extra BrowserRouter needed
+      <App />
     </Provider>
   </React.StrictMode>,
 )
